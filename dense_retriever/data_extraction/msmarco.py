036f5e114ccfb471cb4f4686af639111d2a09bf7
Maxim Razhev <cvlt.mao@gmail.com>
2021-07-19 11:35:01 +0500

    Add text truncation to msmarco to avoid pyarrow memory problems

--- dense_retriever/data_extraction/msmarco.py
+++ dense_retriever/data_extraction/msmarco.py
@@ -16,6 +16,11 @@ c_handler.setFormatter(f_format)
 logger.addHandler(c_handler)
 
 
+def truncate_text(sample, max_len):
+    sample['text'] = sample['text'][:max_len]
+    return sample
+
+
 def extract_sample_text(sample):
     if sample['text'] is None:
         sample['text'] = ''
@@ -23,7 +28,7 @@ def extract_sample_text(sample):
     return {'doc_id': sample['doc_id'], 'text': text}
 
 
-def convert_tsv_to_ndjson(in_file_path, out_file_path, fieldnames, sample_size=None):
+def convert_tsv_to_ndjson(in_file_path, out_file_path, fieldnames, sample_size=None, max_len=None):
     if os.path.exists(out_file_path):
         os.remove(out_file_path)
     with open(in_file_path) as csv_file:
@@ -36,7 +41,10 @@ def convert_tsv_to_ndjson(in_file_path, out_file_path, fieldnames, sample_size=N
             for i, line in tqdm(enumerate(reader)):
                 if sample_size is not None and i > sample_size:
                     break
-                outfile.write(json.dumps(extract_sample_text(line)) + '\n')
+                converted_sample = extract_sample_text(line)
+                if max_len is not None:
+                    converted_sample = truncate_text(converted_sample, max_len)
+                outfile.write(json.dumps(converted_sample) + '\n')
 
 
 @click.command()
@@ -44,7 +52,8 @@ def convert_tsv_to_ndjson(in_file_path, out_file_path, fieldnames, sample_size=N
 @click.argument('out_file', type=str)
 @click.option('-f', '--field-names', type=str, default='doc_id,url,title,text')
 @click.option('-s', '--sample-size', type=int, default=None)
-def convert_msmarco(input_file, out_file, field_names, sample_size):
+@click.option('-m', '--max-len', type=int, default=None)
+def convert_msmarco(input_file, out_file, field_names, sample_size, max_len):
     field_names = field_names.split(',')
     logger.info(f'Converting {input_file} to {out_file}')
-    convert_tsv_to_ndjson(input_file, out_file, field_names, sample_size)
+    convert_tsv_to_ndjson(input_file, out_file, field_names, sample_size, max_len=max_len)
