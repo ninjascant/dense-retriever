c38c91f870636efa1c20dde1901d95993fa5623f
Maxim Razhev <cvlt.mao@gmail.com>
2021-10-17 11:47:43 +0500

    Add command for encoding cache creation

--- dense_retriever/cli/__init__.py
+++ dense_retriever/cli/__init__.py
@@ -1,7 +1,7 @@
 import click
 from .ann_index import run_search_prebuilt_index_command, run_search_from_scratch_command, \
     run_evaluation_command
-from .preprocessing import construct_query_samples_command
+from .preprocessing import construct_query_samples_command, build_encoding_cache_command
 from .inference import run_inference_command
 from .model_training import train_model_with_refresh_command
 
@@ -17,3 +17,4 @@ run.add_command(run_evaluation_command, 'evaluate_index')
 run.add_command(construct_query_samples_command, 'construct_query_samples')
 run.add_command(run_inference_command, 'run_inference')
 run.add_command(train_model_with_refresh_command, 'train_model_with_refresh')
+run.add_command(build_encoding_cache_command, 'build_encoding_cache')
--- dense_retriever/cli/preprocessing.py
+++ dense_retriever/cli/preprocessing.py
@@ -1,5 +1,6 @@
 import click
 from ..transforms.preprocessing import QuerySampleConstructor
+from ..transforms.encoding_cache import EncodingCacheBuilder
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
@@ -9,3 +10,10 @@ from ..transforms.preprocessing import QuerySampleConstructor
 def construct_query_samples_command(qrel_path, query_path, out_path):
     transformer = QuerySampleConstructor(qrel_path=qrel_path)
     transformer.transform(query_path, out_path)
+
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.argument('encoding_dataset_dir', type=str)
+def build_encoding_cache_command(encoding_dataset_dir):
+    transformer = EncodingCacheBuilder()
+    transformer.transform(encoding_dataset_dir, '')
