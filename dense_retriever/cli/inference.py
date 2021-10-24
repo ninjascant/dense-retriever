a3572b8059c6188c81a2c8f4bd3875ac4b75c0af
Maxim Razhev <cvlt.mao@gmail.com>
2021-10-24 15:46:57 +0500

    Fix inference

--- dense_retriever/cli/inference.py
+++ dense_retriever/cli/inference.py
@@ -4,8 +4,9 @@ from ..pipeline_steps.inference import run_inference
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.argument('model_name_or_path', type=str)
+@click.argument('model_type', type=str)
 @click.argument('dataset_dir', type=str)
 @click.argument('out_dir', type=str)
 @click.option('-i', '--id-col', type=str, default='doc_id')
-def run_inference_command(model_name_or_path, dataset_dir, out_dir, id_col):
-    run_inference(model_name_or_path, dataset_dir, out_dir, id_col)
+def run_inference_command(model_name_or_path, model_type, dataset_dir, out_dir, id_col):
+    run_inference(model_name_or_path, model_type, dataset_dir, out_dir, id_col)
