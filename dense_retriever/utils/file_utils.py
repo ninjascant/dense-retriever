8cc1c999376989451f2fc55b74b230fb1b4d4bda
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 14:16:25 +0000

    Adding tarfile member sanitization to extractall()

--- dense_retriever/utils/file_utils.py
+++ dense_retriever/utils/file_utils.py
@@ -12,7 +12,26 @@ def zip_dir(dir_path, archive_path):
 
 def unzip_arch(archive_path):
     with tarfile.open(archive_path, "r:gz") as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
 
 
 def read_jsonl_file(file_path):
